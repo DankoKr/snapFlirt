509636a5185d5c8029f3149c76b724dc82828e28
DankoKr <dankokralski776@gmail.com>
2024-12-17 20:21:49 +0100

    feat: Added CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 from app.utils.logger import setup_logging
+from app.utils.config import FRONTEND_URL
 
 setup_logging()
 
 app = FastAPI(title="Flirty Comment Generator API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=FRONTEND_URL,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"], 
+)
+
 app.include_router(router)
 
 @app.get("/")
--- app/utils/config.py
+++ app/utils/config.py
@@ -7,3 +7,4 @@ GROQ_API_URL = os.getenv("GROQ_API_URL")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_HEADERS = {"Authorization": f"Bearer {GROQ_API_KEY}", "Content-Type": "application/json"}
 MODEL_NAME = "llama-3.1-8b-instant"
+FRONTEND_URL = os.getenv("FRONTEND_URL")
